a69ef3e5b07747cd4dbea51558ed4d3a3f18dc17
vmillozza <142731369+vmillozza@users.noreply.github.com>
2023-09-18 15:28:28 +0200

    Grid Products Insert

--- forms/frmprodotti.py
+++ forms/frmprodotti.py
@@ -3,7 +3,24 @@ from tkinter import END, Entry, Listbox
 
 
 from database import connector as connector
-
+def aggiungi(root):
+    e=tk.Label(root,width=10,text='Nome',borderwidth=2, relief='ridge',anchor='w',bg='yellow')
+    e.grid(row=0,column=0)
+    e=tk.Label(root,width=10,text='Codice',borderwidth=2, relief='ridge',anchor='w',bg='yellow')
+    e.grid(row=0,column=1)
+    e=tk.Label(root,width=10,text='Quantità',borderwidth=2, relief='ridge',anchor='w',bg='yellow')
+    e.grid(row=0,column=2)
+    e=tk.Label(root,width=10,text='Prezzo',borderwidth=2, relief='ridge',anchor='w',bg='yellow')
+    e.grid(row=0,column=3)
+    i=1 
+    products = []
+    for product in products: 
+        for j in range(len(product)):
+            e = Entry(root, width=50, fg='blue') 
+            e.grid(row=i, column=j) 
+            e.insert(END, product[j])
+        i=i+1
+    
 def display_products():
     # Lista di prodotti (puoi sostituirla con i dati provenienti da un database o da un'altra fonte)
     products = connector.elenco_prodotti()
@@ -30,11 +47,13 @@ def display_products():
             e.grid(row=i, column=j) 
             e.insert(END, product[j])
         i=i+1
-    
+    # Crea e posiziona il pulsante di login
+    add_button = tk.Button(root, text="Aggiungi Prodotto", command=aggiungi(root))
+    add_button.pack(pady=20)
     # Avvia il loop principale di tkinter
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-    window_width = 900
+    window_width = 1200
     window_height = 500
     # Calculate the x and y coordinates to center the window
     x = (screen_width / 2) - (window_width / 2)
