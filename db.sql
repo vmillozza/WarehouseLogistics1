74efcde67b23f46fa5be7dca5fc91fdd8e5a3709
vmillozza <142731369+vmillozza@users.noreply.github.com>
2023-09-20 15:30:00 +0200

    Using sqllite

--- db.sql
+++ db.sql
@@ -1,13 +1,13 @@
 -- Creazione della tabella Utenti
 CREATE TABLE Utenti (
-    userID INT AUTO_INCREMENT PRIMARY KEY,
+    userID INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
 -- Creazione della tabella Prodotti
 CREATE TABLE Prodotti (
-    prodottoID INT AUTO_INCREMENT PRIMARY KEY,
+    prodottoID INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_prodotto VARCHAR(255) NOT NULL,
     codice VARCHAR(255) NOT NULL,
     quantità INT NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE Prodotti (
 
 -- Creazione della tabella Ordini
 CREATE TABLE Ordini (
-    ordineID INT AUTO_INCREMENT PRIMARY KEY,
+    ordineID INTEGER PRIMARY KEY AUTOINCREMENT,
     prodottoID INT,
     quantità_ordinata INT NOT NULL,
     codice_spedizione VARCHAR(255) NOT NULL,
@@ -25,8 +25,17 @@ CREATE TABLE Ordini (
 
 -- Creazione della tabella Notifiche
 CREATE TABLE Notifiche (
-    notificaID INT AUTO_INCREMENT PRIMARY KEY,
+    notificaID INTEGER PRIMARY KEY AUTOINCREMENT,
     messaggio TEXT NOT NULL,
     data_ora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-python in
+INSERT INTO Utenti (username, password)
+VALUES ('your_username', 'your_password');
+INSERT INTO Utenti (username, password)
+VALUES ('lallo', 'password');
+INSERT INTO Prodotti (nome_prodotto, codice, quantità, prezzo) 
+VALUES ('Pc', '1aswert', 2, 3.5);
+INSERT INTO Prodotti (nome_prodotto, codice, quantità, prezzo) 
+VALUES ('scanner', '1aswert3a', 1, 3.5);
+INSERT INTO Ordini (prodottoID, quantità_ordinata, codice_spedizione)
+VALUES (1, 10, 'ABC12345');
